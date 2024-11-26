73b873d6e356843758c5bced4c08f01e0b6ba475
mickeycookie <richardson0705@yahoo.com>
2024-11-26 18:22:44 -0500

    .

--- DR - Find The Exit/Assets/PlayerMovement.cs
+++ DR - Find The Exit/Assets/PlayerMovement.cs
@@ -14,7 +14,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 destination = new Vector3(horizontal, 0, vertical);
+        GetComponent<Rigidbody>().velocity = destination * speed;
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
     }
 }
